b7683522820b38d3d48948bccfb52e8694b53553
Brendan Campbell-Hartzell <platinumdoodles@gmail.com>
2020-04-08 11:46:00 -0700

    Remove memory leak by watching for tab change instead of undock

--- infrastructure/src/main/kotlin/com/soyle/stories/characterarc/baseStoryStructure/BaseStoryStructureScope.kt
+++ infrastructure/src/main/kotlin/com/soyle/stories/characterarc/baseStoryStructure/BaseStoryStructureScope.kt
@@ -5,22 +5,35 @@
  */
 package com.soyle.stories.characterarc.baseStoryStructure
 
+import com.soyle.stories.common.launchTask
 import com.soyle.stories.di.characterarc.BaseStoryStructureComponent
 import com.soyle.stories.project.ProjectScope
+import javafx.beans.property.SimpleBooleanProperty
+import javafx.event.EventTarget
 import kotlinx.coroutines.runBlocking
-import tornadofx.Scope
-import tornadofx.find
-import tornadofx.runAsync
+import tornadofx.*
 
 class BaseStoryStructureScope(val projectScope: ProjectScope, val characterId: String, val themeId: String) : Scope() {
 
+    private val isClosedProperty = SimpleBooleanProperty(false)
+    private var isClosed
+        get() = isClosedProperty.get()
+        set(value) {
+            if (isClosedProperty.value) return
+            isClosedProperty.set(value)
+        }
+
     init {
         val baseStoryStructureViewListener = find<BaseStoryStructureComponent>(scope = this).baseStoryStructureViewListener
-        runAsync {
-            runBlocking {
-                baseStoryStructureViewListener.getBaseStoryStructure()
-            }
+        baseStoryStructureViewListener.getBaseStoryStructure()
+    }
+
+    fun close() {
+        FX.getComponents(this).values.forEach {
+            if (it is EventTarget) it.removeFromParent()
         }
+        isClosed = true
+        deregister()
     }
 
 }
