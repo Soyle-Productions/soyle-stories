b38d7cb6ae31eb34acaab5ff4f27e55388bd1c1f
Brendan Campbell-Hartzell <platinumdoodles@gmail.com>
2020-04-11 12:59:37 -0700

    Code clean up.
    Refactored classes to have single responsibility.
    Extracted some strings into new view models and presenters.

--- gui/src/main/kotlin/com/soyle/stories/project/projectList/ProjectListPresenter.kt
+++ gui/src/main/kotlin/com/soyle/stories/project/projectList/ProjectListPresenter.kt
@@ -1,84 +1,96 @@
 package com.soyle.stories.project.projectList
 
 import com.soyle.stories.eventbus.Notifier
+import com.soyle.stories.eventbus.listensTo
+import com.soyle.stories.project.eventbus.ProjectEvents
+import com.soyle.stories.project.projectList.presenters.CloseProjectPresenter
+import com.soyle.stories.project.projectList.presenters.OpenProjectPresenter
+import com.soyle.stories.project.projectList.presenters.StartNewLocalProjectPresenter
 import com.soyle.stories.project.usecases.startnewLocalProject.StartNewLocalProject
 import com.soyle.stories.workspace.ExpectedProjectDoesNotExistAtLocation
+import com.soyle.stories.workspace.ExpectedProjectException
 import com.soyle.stories.workspace.UnexpectedProjectExistsAtLocation
 import com.soyle.stories.workspace.usecases.listOpenProjects.ListOpenProjects
 import com.soyle.stories.workspace.usecases.openProject.OpenProject
 import com.soyle.stories.workspace.usecases.requestCloseProject.RequestCloseProject
 
 class ProjectListPresenter(
-    private val view: ProjectListView,
-    openProjectNotifier: Notifier<OpenProject.OutputPort>,
-    closeProjectNotifier: Notifier<RequestCloseProject.OutputPort>,
-    startNewProjectNotifier: Notifier<StartNewLocalProject.OutputPort>
+  private val view: ProjectListView,
+  projectEvents: ProjectEvents
 ) : ListOpenProjects.OutputPort {
 
-    private val closeProjectOutputPort: RequestCloseProject.OutputPort = CloseProjectPresenter(view)
-    private val openProjectOutputPort: OpenProject.OutputPort = OpenProjectPresenter(view, closeProjectOutputPort)
-    private val startNewLocalProjectOutputPort: StartNewLocalProject.OutputPort =
-        StartNewLocalProjectPresenter(openProjectOutputPort, closeProjectOutputPort)
+	// the sub presenters should not be GC'd until the project list presenter is GC'd, but notifiers hold weak references
+	// to their listeners, so we have to hold strong references here to make sure the sub presenters are not GC'd
+	// prematurely.
+	private val subPresenters: List<Any>
 
-    init {
-        openProjectNotifier.addListener(startNewLocalProjectOutputPort)
-        closeProjectNotifier.addListener(closeProjectOutputPort)
-        startNewProjectNotifier.addListener(startNewLocalProjectOutputPort)
-        //resolveFailedProjectNotifier.addListener(this)
-    }
+	init {
+		val closeProjectOutputPort: RequestCloseProject.OutputPort = CloseProjectPresenter(view)
+		val openProjectOutputPort: OpenProject.OutputPort = OpenProjectPresenter(view, closeProjectOutputPort)
 
-    override fun receiveListOpenProjectsResponse(response: ListOpenProjects.ResponseModel) {
-        view.update {
-            this?.copy(
-                isSplashScreenVisible = false,
-                isWelcomeScreenVisible = response.openProjects.isEmpty() && response.failedProjects.isEmpty(),
-                openProjects = response.openProjects.map {
-                    ProjectFileViewModel(
-                        it.projectId,
-                        it.projectName,
-                        it.projectLocation
-                    )
-                },
-                failedProjects = response.failedProjects.map {
-                    when (it) {
-                        is ExpectedProjectDoesNotExistAtLocation -> ProjectIssueViewModel(
-                            it.expectedProjectName,
-                            it.location
-                        )
-                        is UnexpectedProjectExistsAtLocation -> ProjectIssueViewModel(
-                            it.expectedProjectName,
-                            it.location,
-                            "Found unexpected project: ${it.foundProjectName}"
-                        )
-                    }
-                }
-            )
-                ?: ProjectListViewModel(
-                    openProjectRequest = null,
-                    isSplashScreenVisible = false,
-                    isWelcomeScreenVisible = response.openProjects.isEmpty() && response.failedProjects.isEmpty(),
-                    openProjects = response.openProjects.map {
-                        ProjectFileViewModel(
-                            it.projectId,
-                            it.projectName,
-                            it.projectLocation
-                        )
-                    },
-                    failedProjects = response.failedProjects.map {
-                        when (it) {
-                            is ExpectedProjectDoesNotExistAtLocation -> ProjectIssueViewModel(
-                                it.expectedProjectName,
-                                it.location
-                            )
-                            is UnexpectedProjectExistsAtLocation -> ProjectIssueViewModel(
-                                it.expectedProjectName,
-                                it.location,
-                                "Found unexpected project: ${it.foundProjectName}"
-                            )
-                        }
-                    },
-                    closeProjectRequest = null
-                )
-        }
-    }
+		val startNewLocalProjectOutputPort: StartNewLocalProject.OutputPort =
+		  StartNewLocalProjectPresenter(view, openProjectOutputPort, closeProjectOutputPort)
+
+		subPresenters = listOf(
+		  startNewLocalProjectOutputPort listensTo projectEvents.openProject,
+		  closeProjectOutputPort listensTo projectEvents.closeProject,
+		  startNewLocalProjectOutputPort listensTo projectEvents.startNewProject
+		)
+	}
+
+	override fun receiveListOpenProjectsResponse(response: ListOpenProjects.ResponseModel) {
+		view.update {
+			updateViewModelOrDefault(this, response)
+		}
+	}
+
+	private fun updateViewModelOrDefault(viewModel: ProjectListViewModel?, response: ListOpenProjects.ResponseModel): ProjectListViewModel {
+		return if (viewModel != null) {
+			updateViewModelWithResponse(viewModel, response)
+		} else {
+			defaultProjectListModel(response)
+		}
+	}
+
+	private fun updateViewModelWithResponse(viewModel: ProjectListViewModel, response: ListOpenProjects.ResponseModel): ProjectListViewModel {
+		return viewModel.copy(
+		  isSplashScreenVisible = false,
+		  isWelcomeScreenVisible = response.openProjects.isEmpty() && response.failedProjects.isEmpty(),
+		  openProjects = response.openProjects.map(::convertOpenProjectItemToViewModel),
+		  failedProjects = response.failedProjects.map(::convertExpectedProjectExceptionToViewModel),
+		  startProjectFailure = null
+		)
+	}
+
+	private fun defaultProjectListModel(response: ListOpenProjects.ResponseModel) = ProjectListViewModel(
+	  openProjectRequest = null,
+	  isSplashScreenVisible = false,
+	  isWelcomeScreenVisible = response.openProjects.isEmpty() && response.failedProjects.isEmpty(),
+	  openProjects = response.openProjects.map(::convertOpenProjectItemToViewModel),
+	  failedProjects = response.failedProjects.map(::convertExpectedProjectExceptionToViewModel),
+	  closeProjectRequest = null,
+	  startProjectFailure = null
+	)
+
+	private fun convertExpectedProjectExceptionToViewModel(it: ExpectedProjectException): ProjectIssueViewModel {
+		return when (it) {
+			is ExpectedProjectDoesNotExistAtLocation -> ProjectIssueViewModel(
+			  it.expectedProjectName,
+			  it.location
+			)
+			is UnexpectedProjectExistsAtLocation -> ProjectIssueViewModel(
+			  it.expectedProjectName,
+			  it.location,
+			  "Found unexpected project: ${it.foundProjectName}"
+			)
+		}
+	}
+
+	private fun convertOpenProjectItemToViewModel(item: ListOpenProjects.OpenProjectItem): ProjectFileViewModel {
+		return ProjectFileViewModel(
+		  item.projectId,
+		  item.projectName,
+		  item.projectLocation
+		)
+	}
 }
