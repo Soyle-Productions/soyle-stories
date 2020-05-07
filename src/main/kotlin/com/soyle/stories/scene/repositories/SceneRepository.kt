1a4f58bca3bb796689233b62e4f85224a8d6df4e
Brendan Campbell-Hartzell <platinumdoodles@gmail.com>
2020-05-07 16:25:15 -0700

    Add List All Scenes in Project

--- src/main/kotlin/com/soyle/stories/scene/repositories/SceneRepository.kt
+++ src/main/kotlin/com/soyle/stories/scene/repositories/SceneRepository.kt
@@ -1,9 +1,11 @@
 package com.soyle.stories.scene.repositories
 
+import com.soyle.stories.entities.Project
 import com.soyle.stories.entities.Scene
 
 interface SceneRepository {
 
 	suspend fun createNewScene(scene: Scene)
+	suspend fun listAllScenesInProject(projectId: Project.Id): List<Scene>
 
 }
