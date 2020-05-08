c734dbf210de8c1306a07019914758713019a34a
Brendan Campbell-Hartzell <platinumdoodles@gmail.com>
2020-05-08 15:18:27 -0700

    Can't delete scenes through scene list

--- gui/src/main/kotlin/com/soyle/stories/scene/sceneList/SceneListController.kt
+++ gui/src/main/kotlin/com/soyle/stories/scene/sceneList/SceneListController.kt
@@ -1,7 +1,6 @@
 package com.soyle.stories.scene.sceneList
 
 import com.soyle.stories.gui.ThreadTransformer
-import com.soyle.stories.scene.deleteScene.DeleteSceneController
 import com.soyle.stories.scene.renameScene.RenameSceneController
 import com.soyle.stories.scene.usecases.listAllScenes.ListAllScenes
 
@@ -9,8 +8,7 @@ class SceneListController(
   private val threadTransformer: ThreadTransformer,
   private val listAllScenes: ListAllScenes,
   private val listAllScenesOutputPort: ListAllScenes.OutputPort,
-  private val renameSceneController: RenameSceneController,
-  private val deleteSceneController: DeleteSceneController
+  private val renameSceneController: RenameSceneController
 ) : SceneListViewListener {
 	override fun getValidState() {
 		threadTransformer.async {
@@ -19,7 +17,6 @@ class SceneListController(
 	}
 
 	override fun openSceneDetails(sceneId: String) {
-		deleteSceneController.deleteScene(sceneId)
 	}
 
 	override fun renameScene(sceneId: String, newName: String) {
