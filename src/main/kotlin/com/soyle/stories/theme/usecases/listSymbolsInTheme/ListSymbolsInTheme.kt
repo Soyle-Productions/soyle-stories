62d8738d1edea23531e1138be6b4ac73ef3eb301
Brendan Campbell-Hartzell <50385408+b-camphart@users.noreply.github.com>
2020-07-03 09:44:36 -0700

    Add "List Symbols in Theme" use case

---
+++ src/main/kotlin/com/soyle/stories/theme/usecases/listSymbolsInTheme/ListSymbolsInTheme.kt
@@ -0,0 +1,13 @@
+package com.soyle.stories.theme.usecases.listSymbolsInTheme
+
+import java.util.*
+
+interface ListSymbolsInTheme {
+
+    suspend operator fun invoke(themeId: UUID, output: OutputPort)
+
+    interface OutputPort {
+        suspend fun symbolsListedInTheme(response: SymbolsInTheme)
+    }
+
+}
