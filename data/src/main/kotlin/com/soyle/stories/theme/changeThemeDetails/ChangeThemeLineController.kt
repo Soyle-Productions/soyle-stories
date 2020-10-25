751c3c0b4ebcbf3f59f500adc09d2c46fd238a04
Brendan Campbell-Hartzell <50385408+b-camphart@users.noreply.github.com>
2020-10-25 10:10:01 -0700

    Add Controller for Change Theme Line

---
+++ data/src/main/kotlin/com/soyle/stories/theme/changeThemeDetails/ChangeThemeLineController.kt
@@ -0,0 +1,7 @@
+package com.soyle.stories.theme.changeThemeDetails
+
+interface ChangeThemeLineController {
+
+    fun changeThemeLine(themeId: String, themeLine: String)
+
+}
---
+++ data/src/main/kotlin/com/soyle/stories/theme/changeThemeDetails/ChangeThemeLineControllerImpl.kt
@@ -0,0 +1,23 @@
+package com.soyle.stories.theme.changeThemeDetails
+
+import com.soyle.stories.common.ThreadTransformer
+import com.soyle.stories.theme.usecases.changeThemeDetails.ChangeThemeLine
+import java.util.*
+
+class ChangeThemeLineControllerImpl(
+    private val threadTransformer: ThreadTransformer,
+    private val changeThemeLine: ChangeThemeLine,
+    private val changeThemeLineOutput: ChangeThemeLine.OutputPort
+) : ChangeThemeLineController {
+
+    override fun changeThemeLine(themeId: String, themeLine: String) {
+        val preparedThemeId = UUID.fromString(themeId)
+        threadTransformer.async {
+            changeThemeLine.invoke(
+                preparedThemeId,
+                themeLine,
+                changeThemeLineOutput
+            )
+        }
+    }
+}
