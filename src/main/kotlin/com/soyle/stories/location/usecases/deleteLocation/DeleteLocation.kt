cf250b37c6c144d96f1a58cbf8b469236df63583
Brendan Campbell-Hartzell <50385408+b-camphart@users.noreply.github.com>
2021-01-10 17:13:33 -0800

    Extract Deleted Location event from DeleteLocation use case response model

--- src/main/kotlin/com/soyle/stories/location/usecases/deleteLocation/DeleteLocation.kt
+++ src/main/kotlin/com/soyle/stories/location/usecases/deleteLocation/DeleteLocation.kt
@@ -1,15 +1,16 @@
 package com.soyle.stories.location.usecases.deleteLocation
 
-import com.soyle.stories.location.LocationException
 import java.util.*
 
 interface DeleteLocation {
 	suspend operator fun invoke(id: UUID, output: OutputPort)
 
-	class ResponseModel(val locationId: UUID, val updatedArcSections: Set<UUID>)
+	class ResponseModel(
+		val deletedLocation: DeletedLocation,
+		val updatedArcSections: Set<UUID>
+		)
 
 	interface OutputPort {
-		fun receiveDeleteLocationFailure(failure: LocationException)
-		fun receiveDeleteLocationResponse(response: ResponseModel)
+		suspend fun receiveDeleteLocationResponse(response: ResponseModel)
 	}
 }
