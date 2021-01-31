64b05187d6f36f5441aded8e4e68b8eb484f51df
Brendan Campbell-Hartzell <50385408+b-camphart@users.noreply.github.com>
2021-01-31 00:23:18 -0800

    Ensure Listed Symbols can be Pinned in the Scene

--- src/main/kotlin/com/soyle/stories/scene/usecases/trackSymbolInScene/PinSymbolToSceneUseCase.kt
+++ src/main/kotlin/com/soyle/stories/scene/usecases/trackSymbolInScene/PinSymbolToSceneUseCase.kt
@@ -5,16 +5,30 @@ import com.soyle.stories.entities.Updated
 import com.soyle.stories.entities.theme.Symbol
 import com.soyle.stories.scene.repositories.SceneRepository
 import com.soyle.stories.scene.repositories.getSceneOrError
+import com.soyle.stories.theme.SymbolDoesNotExist
+import com.soyle.stories.theme.repositories.ThemeRepository
 
 class PinSymbolToSceneUseCase(
-    private val sceneRepository: SceneRepository
+    private val sceneRepository: SceneRepository,
+    private val themeRepository: ThemeRepository
 ) : PinSymbolToScene {
     override suspend fun invoke(sceneId: Scene.Id, symbolId: Symbol.Id, output: PinSymbolToScene.OutputPort) {
         val scene = sceneRepository.getSceneOrError(sceneId.uuid)
-        val sceneUpdate = scene.withSymbolPinned(symbolId)
-        if (sceneUpdate is Updated) {
-            sceneRepository.updateScene(sceneUpdate.scene)
-            output.symbolPinnedToScene(PinSymbolToScene.ResponseModel(sceneUpdate.event))
+        if (scene.trackedSymbols.isSymbolTracked(symbolId)) {
+            val sceneUpdate = scene.withSymbolPinned(symbolId)
+            if (sceneUpdate is Updated) {
+                sceneRepository.updateScene(sceneUpdate.scene)
+                output.symbolPinnedToScene(PinSymbolToScene.ResponseModel(sceneUpdate.event, null))
+            }
+        } else {
+            val theme = themeRepository.getThemeContainingSymbolWithId(symbolId)
+                ?: throw SymbolDoesNotExist(symbolId.uuid)
+            val symbol = theme.symbols.find { it.id == symbolId }!!
+            val sceneUpdate = scene.withSymbolTracked(theme, symbol, true)
+            if (sceneUpdate is Updated) {
+                sceneRepository.updateScene(sceneUpdate.scene)
+                output.symbolPinnedToScene(PinSymbolToScene.ResponseModel(null, sceneUpdate.event))
+            }
         }
     }
 }
--- src/test/kotlin/com/soyle/stories/scene/usecases/Pin Symbol to Scene Unit Test.kt
+++ src/test/kotlin/com/soyle/stories/scene/usecases/Pin Symbol to Scene Unit Test.kt
@@ -1,16 +1,18 @@
 package com.soyle.stories.scene.usecases
 
 import com.soyle.stories.common.mustEqual
+import com.soyle.stories.doubles.ThemeRepositoryDouble
 import com.soyle.stories.entities.Scene
 import com.soyle.stories.scene.SceneDoesNotExist
 import com.soyle.stories.scene.doubles.SceneRepositoryDouble
 import com.soyle.stories.scene.makeScene
 import com.soyle.stories.scene.usecases.trackSymbolInScene.PinSymbolToScene
 import com.soyle.stories.scene.usecases.trackSymbolInScene.PinSymbolToSceneUseCase
+import com.soyle.stories.theme.SymbolDoesNotExist
 import com.soyle.stories.theme.makeSymbol
 import com.soyle.stories.theme.makeTheme
 import kotlinx.coroutines.runBlocking
-import org.junit.jupiter.api.Assertions.assertNull
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
@@ -20,12 +22,12 @@ class `Pin Symbol to Scene Unit Test` {
     private val symbol = makeSymbol()
     private val theme = makeTheme(symbols = listOf(symbol))
     private val scene = makeScene()
-        .withSymbolTracked(theme, symbol).scene
 
     private var updatedScene: Scene? = null
     private var result: PinSymbolToScene.ResponseModel? = null
 
     private val sceneRepository = SceneRepositoryDouble(onUpdateScene = ::updatedScene::set)
+    private val themeRepository = ThemeRepositoryDouble()
 
     @Test
     fun `scene doesn't exist`() {
@@ -43,39 +45,81 @@ class `Pin Symbol to Scene Unit Test` {
             sceneRepository.givenScene(scene)
         }
 
-        @Test
-        fun `should update scene`() {
-            pinSymbolToScene()
-            updatedScene!!.trackedSymbols.getSymbolByIdOrError(symbol.id).isPinned.mustEqual(true)
-        }
-
-        @Test
-        fun `should output event`() {
-            pinSymbolToScene()
-            result!!.symbolPinnedToScene.let {
-                it.sceneId.mustEqual(scene.id)
-                it.trackedSymbol.mustEqual(updatedScene!!.trackedSymbols.getSymbolByIdOrError(symbol.id))
-            }
-        }
-
         @Nested
-        inner class `Given symbol is already pinned`
+        inner class `Given symbol is tracked`
         {
 
             init {
-                sceneRepository.givenScene(scene.withSymbolPinned(symbol.id).scene)
+                sceneRepository.givenScene(scene.withSymbolTracked(theme, symbol).scene)
             }
 
             @Test
-            fun `should not update scene`() {
+            fun `should update scene`() {
                 pinSymbolToScene()
-                assertNull(updatedScene)
+                updatedScene!!.trackedSymbols.getSymbolByIdOrError(symbol.id).isPinned.mustEqual(true)
             }
 
             @Test
-            fun `should not produce output`() {
+            fun `should output event`() {
                 pinSymbolToScene()
-                assertNull(result)
+                result!!.symbolPinnedToScene!!.let {
+                    it.sceneId.mustEqual(scene.id)
+                    it.trackedSymbol.mustEqual(updatedScene!!.trackedSymbols.getSymbolByIdOrError(symbol.id))
+                }
+            }
+
+            @Nested
+            inner class `Given symbol is already pinned`
+            {
+
+                init {
+                    sceneRepository.givenScene(scene.withSymbolTracked(theme, symbol, true).scene)
+                }
+
+                @Test
+                fun `should not update scene`() {
+                    pinSymbolToScene()
+                    Assertions.assertNull(updatedScene)
+                }
+
+                @Test
+                fun `should not produce output`() {
+                    pinSymbolToScene()
+                    Assertions.assertNull(result)
+                }
+
+            }
+        }
+
+        @Test
+        fun `symbol doesn't exist`() {
+            val error = assertThrows<SymbolDoesNotExist> {
+                pinSymbolToScene()
+            }
+            error.symbolId.mustEqual(symbol.id.uuid)
+        }
+
+        @Nested
+        inner class `Given symbol exists`
+        {
+
+            init {
+                themeRepository.givenTheme(theme)
+            }
+
+            @Test
+            fun `should track symbol in scene`() {
+                pinSymbolToScene()
+                updatedScene!!.trackedSymbols.getSymbolByIdOrError(symbol.id).isPinned.mustEqual(true)
+            }
+
+            @Test
+            fun `should output event`() {
+                pinSymbolToScene()
+                result!!.symbolTrackedInScene!!.let {
+                    it.sceneId.mustEqual(scene.id)
+                    it.trackedSymbol.mustEqual(updatedScene!!.trackedSymbols.getSymbolByIdOrError(symbol.id))
+                }
             }
 
         }
@@ -83,7 +127,7 @@ class `Pin Symbol to Scene Unit Test` {
     }
 
     private fun pinSymbolToScene() {
-        val useCase: PinSymbolToScene = PinSymbolToSceneUseCase(sceneRepository)
+        val useCase: PinSymbolToScene = PinSymbolToSceneUseCase(sceneRepository, themeRepository)
         val output = object : PinSymbolToScene.OutputPort {
             override suspend fun symbolPinnedToScene(response: PinSymbolToScene.ResponseModel) {
                 result = response
