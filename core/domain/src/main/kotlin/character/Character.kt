e110d160e5fe7cc99310a88c133ae1b1a26ec435
Brendan Campbell-Hartzell <50385408+b-camphart@users.noreply.github.com>
2021-05-18 13:01:13 -0700

    Can rename character name variants

--- core/domain/src/main/kotlin/character/Character.kt
+++ core/domain/src/main/kotlin/character/Character.kt
@@ -1,6 +1,7 @@
 package com.soyle.stories.domain.character
 
 import com.soyle.stories.domain.character.events.CharacterNameVariantAdded
+import com.soyle.stories.domain.character.events.CharacterNameVariantRenamed
 import com.soyle.stories.domain.entities.Entity
 import com.soyle.stories.domain.entities.EntityUpdate
 import com.soyle.stories.domain.media.Media
@@ -9,45 +10,76 @@ import com.soyle.stories.domain.validation.NonBlankString
 import java.util.*
 
 class Character(
-	override val id: Id,
-	val projectId: Project.Id,
-	val name: NonBlankString,
-	val otherNames: Set<NonBlankString>,
-	val media: Media.Id?
+    override val id: Id,
+    val projectId: Project.Id,
+    val name: NonBlankString,
+    val otherNames: Set<NonBlankString>,
+    val media: Media.Id?
 ) : Entity<Character.Id> {
 
-	constructor(projectId: Project.Id, name: NonBlankString, media: Media.Id? = null) : this(Id(), projectId, name, setOf(), media)
+    constructor(projectId: Project.Id, name: NonBlankString, media: Media.Id? = null) : this(
+        Id(),
+        projectId,
+        name,
+        setOf(),
+        media
+    )
 
-	private fun copy(
-		name: NonBlankString = this.name,
-		media: Media.Id? = this.media,
-		otherNames: Set<NonBlankString> = this.otherNames
-	) = Character(id, projectId, name, otherNames, media)
+    private fun copy(
+        name: NonBlankString = this.name,
+        media: Media.Id? = this.media,
+        otherNames: Set<NonBlankString> = this.otherNames
+    ) = Character(id, projectId, name, otherNames, media)
 
-	fun withName(name: NonBlankString): Character = copy(name = name)
+    fun withName(name: NonBlankString): Character = copy(name = name)
 
-	/*
-	Should add the variant to the list of other names, unless it is the same as the name or one of the other names.
-	 */
-	fun withNameVariant(variant: NonBlankString): CharacterUpdate<CharacterNameVariantAdded>
-	{
-		if (variant == name) return noUpdate(reason = CharacterNameVariantCannotEqualDisplayName(id, variant.value))
-		if (variant in otherNames) return noUpdate(reason = CharacterNameVariantCannotEqualOtherVariant(id, variant.value))
-		return CharacterUpdate.Updated(
-			character = copy(otherNames = otherNames + variant),
-			event = CharacterNameVariantAdded(id, variant.value)
-		)
-	}
+    /*
+    Should add the variant to the list of other names, unless it is the same as the name or one of the other names.
+     */
+    fun withNameVariant(variant: NonBlankString): CharacterUpdate<CharacterNameVariantAdded> {
+        if (variant == name) return noUpdate(reason = CharacterNameVariantCannotEqualDisplayName(id, variant.value))
+        if (variant in otherNames) return noUpdate(
+            reason = CharacterNameVariantCannotEqualOtherVariant(
+                id,
+                variant.value
+            )
+        )
+        return CharacterUpdate.Updated(
+            character = copy(otherNames = otherNames + variant),
+            event = CharacterNameVariantAdded(id, variant.value)
+        )
+    }
 
-	fun noUpdate(reason: Any? = null) = CharacterUpdate.WithoutChange(this, reason)
+    fun withNameVariantModified(
+        currentVariant: NonBlankString,
+        replacement: NonBlankString
+    ): CharacterUpdate<CharacterNameVariantRenamed> {
+        if (currentVariant == replacement) return noUpdate()
+        if (replacement == name) return noUpdate(reason = CharacterNameVariantCannotEqualDisplayName(id, replacement.value))
+        if (replacement in otherNames) return noUpdate(
+            reason = CharacterNameVariantCannotEqualOtherVariant(
+                id,
+                replacement.value
+            )
+        )
 
-	data class Id(val uuid: UUID = UUID.randomUUID()) {
-		override fun toString(): String = "Character($uuid)"
-	}
+        return CharacterUpdate.Updated(
+            character = copy(otherNames = otherNames - currentVariant + replacement),
+            event = CharacterNameVariantRenamed(id, currentVariant, replacement)
+        )
+    }
 
-	companion object {
-		fun buildNewCharacter(projectId: Project.Id, name: NonBlankString): Character = Character(projectId, name)
-	}
+    fun noUpdate(reason: Any? = null) = CharacterUpdate.WithoutChange(this, reason)
+
+    data class Id(val uuid: UUID = UUID.randomUUID()) {
+
+        override fun toString(): String = "Character($uuid)"
+    }
+
+    companion object {
+
+        fun buildNewCharacter(projectId: Project.Id, name: NonBlankString): Character = Character(projectId, name)
+    }
 }
 
 class CharacterRenamed(val characterId: Character.Id, val newName: String)
--- core/domain/src/test/kotlin/character/CharacterTest.kt
+++ core/domain/src/test/kotlin/character/CharacterTest.kt
@@ -1,60 +1,107 @@
 package com.soyle.stories.domain.character
 
+import com.soyle.stories.domain.character.events.CharacterNameVariantRenamed
 import com.soyle.stories.domain.mustEqual
 import com.soyle.stories.domain.project.Project
 import com.soyle.stories.domain.validation.NonBlankString
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 class CharacterTest {
 
-	val projectId = Project.Id()
+    val projectId = Project.Id()
+
+    @Test
+    fun `characters are built`() {
+        Character.buildNewCharacter(projectId, NonBlankString.create("Bob")!!)
+    }
+
+    @Test
+    fun `characters can be renamed`() {
+        val character = Character.buildNewCharacter(projectId, NonBlankString.create("Bob")!!)
+            .withName(NonBlankString.create("Frank")!!)
+        assertEquals("Frank", character.name.value)
+    }
+
+    @Test
+    fun `can create a new name variant`() {
+        val character = makeCharacter()
+        val update = character.withNameVariant(NonBlankString.create("Frank")!!)
+        update as CharacterUpdate.Updated
+        update.event.characterId.mustEqual(character.id)
+        update.event.newVariant.mustEqual("Frank")
+        update.character.otherNames.find { it.value == "Frank" }!!
+    }
+
+    @Test
+    fun `can create a second name variant`() {
+        val character = makeCharacter()
+        val update = character.withNameVariant(NonBlankString.create("Frank")!!)
+            .character.withNameVariant(NonBlankString.create("Fred")!!)
+        update.character.otherNames.contains(NonBlankString.create("Frank")!!).mustEqual(true)
+        update.character.otherNames.contains(NonBlankString.create("Fred")!!).mustEqual(true)
+    }
+
+    @Test
+    fun `name variant cannot be the same as the display name`() {
+        val character = makeCharacter(name = NonBlankString.create("Frank")!!)
+        val update = character.withNameVariant(NonBlankString.create("Frank")!!)
+        update as CharacterUpdate.WithoutChange
+        update.reason.mustEqual(CharacterNameVariantCannotEqualDisplayName(character.id, "Frank"))
+    }
+
+    @Test
+    fun `name variant cannot be the same another name variant`() {
+        val character = makeCharacter()
+        val update = character.withNameVariant(NonBlankString.create("Frank")!!)
+            .character.withNameVariant(NonBlankString.create("Frank")!!)
+        update as CharacterUpdate.WithoutChange
+        update.reason.mustEqual(CharacterNameVariantCannotEqualOtherVariant(character.id, "Frank"))
+    }
+
+    @Test
+    fun `can modify a name variant`() {
+        val originalVariant = NonBlankString.create("Frank")!!
+        val newVariant = NonBlankString.create("George")!!
+        val character = makeCharacter().withNameVariant(originalVariant).character
+        val update = character.withNameVariantModified(originalVariant, newVariant)
+        update as CharacterUpdate.Updated
+
+        update.event.mustEqual(CharacterNameVariantRenamed(character.id, originalVariant, newVariant))
+        update.character.otherNames.single { it == newVariant }
+        assertFalse(update.character.otherNames.contains(originalVariant))
+    }
 
 	@Test
-	fun `characters are built`() {
-		Character.buildNewCharacter(projectId, NonBlankString.create("Bob")!!)
-	}
+	fun `cannot modify name variant to be same as display name`() {
+		val originalVariant = NonBlankString.create("Frank")!!
+		val character = makeCharacter().withNameVariant(originalVariant).character
+		val update = character.withNameVariantModified(originalVariant, character.name)
 
-	@Test
-	fun `characters can be renamed`() {
-		val character = Character.buildNewCharacter(projectId, NonBlankString.create("Bob")!!)
-		  .withName(NonBlankString.create("Frank")!!)
-		assertEquals("Frank", character.name.value)
-	}
-
-	@Test
-	fun `can create a new name variant`() {
-		val character = makeCharacter()
-		val update = character.withNameVariant(NonBlankString.create("Frank")!!)
-		update as CharacterUpdate.Updated
-		update.event.characterId.mustEqual(character.id)
-		update.event.newVariant.mustEqual("Frank")
-		update.character.otherNames.find { it.value == "Frank" }!!
-	}
-
-	@Test
-	fun `can create a second name variant`() {
-		val character = makeCharacter()
-		val update = character.withNameVariant(NonBlankString.create("Frank")!!)
-			.character.withNameVariant(NonBlankString.create("Fred")!!)
-		update.character.otherNames.contains(NonBlankString.create("Frank")!!).mustEqual(true)
-		update.character.otherNames.contains(NonBlankString.create("Fred")!!).mustEqual(true)
-	}
-
-	@Test
-	fun `name variant cannot be the same as the display name`() {
-		val character = makeCharacter(name = NonBlankString.create("Frank")!!)
-		val update = character.withNameVariant(NonBlankString.create("Frank")!!)
 		update as CharacterUpdate.WithoutChange
-		update.reason.mustEqual(CharacterNameVariantCannotEqualDisplayName(character.id, "Frank"))
+		update.reason.mustEqual(CharacterNameVariantCannotEqualDisplayName(character.id, character.name.value))
 	}
 
 	@Test
-	fun `name variant cannot be the same another name variant`() {
-		val character = makeCharacter()
-		val update = character.withNameVariant(NonBlankString.create("Frank")!!)
-			.character.withNameVariant(NonBlankString.create("Frank")!!)
+	fun `cannot modify name variant to be the same as another name variant`() {
+		val originalVariant = NonBlankString.create("Frank")!!
+		val newVariant = NonBlankString.create("George")!!
+		val character = makeCharacter().withNameVariant(originalVariant)
+			.character.withNameVariant(newVariant)
+			.character
+		val update = character.withNameVariantModified(originalVariant, newVariant)
+
 		update as CharacterUpdate.WithoutChange
-		update.reason.mustEqual(CharacterNameVariantCannotEqualOtherVariant(character.id, "Frank"))
+		update.reason.mustEqual(CharacterNameVariantCannotEqualOtherVariant(character.id, "George"))
+	}
+
+	@Test
+	fun `modifying name variant without a change should produce no update`() {
+		val originalVariant = NonBlankString.create("Frank")!!
+		val character = makeCharacter().withNameVariant(originalVariant).character
+		val update = character.withNameVariantModified(originalVariant, originalVariant)
+
+		update as CharacterUpdate.WithoutChange
+		assertNull(update.reason)
 	}
 }
