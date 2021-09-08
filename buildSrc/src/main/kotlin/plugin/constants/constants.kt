108136779dc0bdcfe73e439eeabc323c17fb3160
Brendan Campbell-Hartzell <50385408+b-camphart@users.noreply.github.com>
2021-09-08 11:25:30 -0700

    Upgrade to javafx plugin 0.0.10

--- buildSrc/src/main/kotlin/plugin/constants/constants.kt
+++ buildSrc/src/main/kotlin/plugin/constants/constants.kt
@@ -10,7 +10,7 @@ const val ideaExt = "org.jetbrains.gradle.plugin.idea-ext"
 const val ideaExtVersion = "0.10"
 
 const val javafx = "org.openjfx.javafxplugin"
-const val javaFxVersion = "0.0.9"
+const val javaFxVersion = "0.0.10"
 
 const val shadow = "com.github.johnrengelman.shadow"
 const val shadowVersion = "6.1.0"
